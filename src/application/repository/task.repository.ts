436da2247c8e57311fc9d41ff90dac7a1521646c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-27 00:13:29 +0300

    fix repository

--- src/application/repository/task.repository.ts
+++ src/application/repository/task.repository.ts
@@ -1,15 +1,32 @@
 import { EntityRepository, RemoveOptions, Repository } from 'typeorm';
 import { Task } from '../entity/task.entity';
 import { NotFoundException } from './not.found.exception';
+import { CreateTaskRequest } from '../../task/request/create.task.request';
+import { v4 as uuidv4 } from 'uuid';
+import { TaskStatusEnum } from '../entity/task.status.enum';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private description: any;
   /**
    * @param entity Task
    */
-  async createTask(entity: Task): Promise<void> {
-    const task = this.create(entity);
-    await this.save(task);
+  async createTask(entity: CreateTaskRequest): Promise<Task> {
+    const { title, description } = entity;
+    const task = this.create({
+      id: uuidv4(),
+      title,
+      description,
+      createAt: new Date(),
+      status: TaskStatusEnum.STATUS_START,
+    });
+
+    try {
+      await task.save();
+    } catch (error) {
+      console.log(error);
+    }
+    return task;
   }
 
   /**
