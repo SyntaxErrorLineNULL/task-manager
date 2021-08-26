0d101fd5ad6c30e1e1938f0ab414023946a76127
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-27 00:14:10 +0300

    fix controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskRequest } from './request/create.task.request';
 import { Task } from '../application/entity/task.entity';
-import { v4 as uuidv4 } from 'uuid';
 
 @Controller('task')
 export class TaskController {
@@ -14,9 +13,7 @@ export class TaskController {
   }
 
   @Post('create')
-  getRequestProperty(@Body() body: CreateTaskRequest) {
-    const task = new Task(uuidv4(), body.title, body.description);
-    this.taskService.createTask(task);
-    return 'Ok';
+  getRequestProperty(@Body() body: CreateTaskRequest): Promise<Task> {
+    return this.taskService.createTask(body);
   }
 }
