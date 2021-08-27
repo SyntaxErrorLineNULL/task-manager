3de63679dd517d03bf1cf47336f6149c3c53cee0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-27 23:01:17 +0300

    create password service

---
+++ src/application/service/password.service.ts
@@ -0,0 +1,23 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+import { bcrypt } from 'bcrypt';
+
+export class PasswordService {
+  rounds = `k>CwKU4W=G|I__1$9CT,.re,\'b<H)I`;
+
+  async hash(password: string) {
+    await bcrypt.hash(password, this.rounds, (error, result) => {
+      error ? new Error('Hash password is not success') : console.log(result);
+    });
+  }
+
+  async validate(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash, (error, result) => {
+      error
+        ? new Error('Compare password is not success')
+        : console.log(result);
+    });
+  }
+}
