7ae9c59119452a65c8121ad851178b4d26034c4e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-28 20:09:36 +0300

    update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,17 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../application/repository/user.repository';
+import { PasswordService } from '../application/service/password.service';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private passwordService: PasswordService,
+  ) {}
 }
