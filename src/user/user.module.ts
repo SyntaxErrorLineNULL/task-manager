fb220a2258b5488c50610079517b7bf75973f8b7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-28 19:55:20 +0300

    create user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Module({
+  providers: [UserService]
+})
+export class UserModule {}
