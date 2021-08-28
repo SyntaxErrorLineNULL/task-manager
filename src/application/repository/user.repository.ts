db3759e10f9421aac75b80d8ec8d6eb2a30d0f1a
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-28 20:04:01 +0300

    create user repository

---
+++ src/application/repository/user.repository.ts
@@ -0,0 +1,25 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entity/user.entity';
+import { NotFoundException } from '@nestjs/common';
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+  /**
+   * @param id
+   */
+  async getTaskById(id: string): Promise<User> {
+    const user = await this.findOne({ where: { id } });
+    if (user === null) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return user;
+  }
+
+  async getAllUser(): Promise<User[]> {
+    return await this.find({ order: { id: 'DESC', createAt: 'ASC' } });
+  }
+}
