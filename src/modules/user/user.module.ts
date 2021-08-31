fb94017e4a287d2059c810fcc3e5617786349495
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-31 23:37:25 +0300

    update module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,11 +5,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from '../application/repository/user.repository';
-import { PasswordService } from '../application/service/password.service';
+import { UserRepository } from '../../application/repository/user.repository';
+import { PasswordService } from '../../application/service/password.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [UserService, PasswordService],
+  controllers: [UserController],
 })
 export class UserModule {}
