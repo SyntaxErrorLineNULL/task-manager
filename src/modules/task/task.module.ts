97f0126e6bc8e68ca2519ef9e39214284331ce85
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-31 23:37:32 +0300

    update module

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -6,7 +6,7 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TaskRepository } from '../application/repository/task.repository';
+import { TaskRepository } from '../../application/repository/task.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TaskRepository])],
