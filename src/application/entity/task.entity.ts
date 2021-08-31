b5910dffec69775eddb760823b114721b68d8978
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-31 19:14:53 +0300

    update entity

--- src/application/entity/task.entity.ts
+++ src/application/entity/task.entity.ts
@@ -4,20 +4,26 @@
 
 import {
   BaseEntity,
+  BeforeInsert,
   BeforeUpdate,
   Column,
+  CreateDateColumn,
   Entity,
-  ManyToMany,
-  ManyToOne,
   PrimaryColumn,
 } from 'typeorm';
 import { TaskStatusEnum } from './task.status.enum';
 import { User } from './user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity()
 export class Task extends BaseEntity {
   @PrimaryColumn({ type: 'uuid' })
   id: string;
+
+  @BeforeInsert()
+  async generateId(): Promise<void> {
+    this.id = await uuidv4();
+  }
 
   @Column({ type: 'text' })
   title: string;
@@ -25,12 +31,16 @@ export class Task extends BaseEntity {
   @Column({ type: 'text' })
   description: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    name: 'createAt',
+  })
   createAt: Date;
 
   @Column({
     type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
+    default: () => 'null',
     nullable: true,
   })
   finishAt?: Date;
@@ -46,11 +56,6 @@ export class Task extends BaseEntity {
     default: TaskStatusEnum.STATUS_START,
   })
   status: TaskStatusEnum;
-
-  @BeforeUpdate()
-  changeStatus(): void {
-    this.status = TaskStatusEnum.STATUS_DONE;
-  }
 
   /*@ManyToOne(() => User, (user) => user.tasks, { eager: false })
   user: User;*/
