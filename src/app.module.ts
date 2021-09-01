b5085b5ba8c6657e0e279fe9b1363457864f070f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-01 13:27:38 +0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from '../config/typeorm.config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CategoryTaskModule } from './modules/category-task/category-task.module';
+import { CategoryTaskController } from './modules/category-task/category-task.controller';
+import TaskEntity from "./application/entity/task.entity";
+import UserEntity from "./application/entity/user.entity";
+import CategoryEntity from "./application/entity/category.entity";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forFeature([TaskEntity, UserEntity, CategoryEntity]),
     TypeOrmModule.forRoot(typeORMConfig),
     TaskModule,
     UserModule,
     AuthModule,
+    CategoryTaskModule,
   ],
+  controllers: [CategoryTaskController],
 })
 export class AppModule {}
