c83fb46639098b4010769e2aa44b9b112db000a1
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-01 16:56:21 +0300

    update category module

--- src/modules/category-task/category-task.controller.ts
+++ src/modules/category-task/category-task.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CategoryTaskService } from './category-task.service';
+import CreateCategoryDto from '../common/dto/create.category.dto';
+import CategoryEntity from '../../application/entity/category.entity';
 
 @Controller('category-task')
-export class CategoryTaskController {}
+export class CategoryTaskController {
+  constructor(private service: CategoryTaskService) {}
+
+  @Post('create')
+  createCategoryTask(@Body() body: CreateCategoryDto): Promise<CategoryEntity> {
+    return this.service.create(body);
+  }
+
+  @Get('category-tasks')
+  getAllCategory(): Promise<CategoryEntity[]> {
+    return this.service.getAll();
+  }
+}
--- src/modules/category-task/category-task.service.ts
+++ src/modules/category-task/category-task.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import CategoryRepository from '../../application/repository/category.repository';
+import CategoryEntity from '../../application/entity/category.entity';
+import CreateCategoryDto from '../common/dto/create.category.dto';
 
 @Injectable()
-export class CategoryTaskService {}
+export class CategoryTaskService {
+  constructor(
+    @InjectRepository(CategoryRepository)
+    private categoryRepository: CategoryRepository,
+  ) {}
+
+  async create(entity: CreateCategoryDto): Promise<CategoryEntity> {
+    const category = this.categoryRepository.create(entity);
+    return await this.categoryRepository.save(category);
+  }
+
+  async getAll(): Promise<CategoryEntity[]> {
+    return await this.categoryRepository.getAll();
+  }
+}
