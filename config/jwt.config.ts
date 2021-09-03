a5f24c61a727ad832884e3963a0926650755142c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-03 19:17:35 +0300

    update jwt config

---
+++ config/jwt.config.ts
@@ -0,0 +1,10 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+export const jwtConfig = {
+  secret: 'hcWsoSeZl9qooEPQgPhkZESIDgxISPgH',
+  jwtExpirationTime: 3600,
+  algorithm: [''],
+  signOptions: { expiresIn: '' },
+};
