27b9746baab7b85c320d7272ef9a5f4a101265e6
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-03 12:32:22 +0300

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerSetup } from '../config/swagger-setup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swaggerSetup(app);
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap().then((r): void => console.log(r));
