84e2ff6d55f2d12faf881b15d4dcb52ac43882a2
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-03 19:20:01 +0300

    method find by email and create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from '../../application/repository/user.repository';
 import { PasswordService } from '../../application/service/password.service';
+import { SignUpDto } from '../common/dto/signUp.dto';
+import UserEntity from '../../application/entity/user.entity';
 
 @Injectable()
 export class UserService {
@@ -14,4 +16,17 @@ export class UserService {
     private userRepository: UserRepository,
     private passwordService: PasswordService,
   ) {}
+
+  public async createUser(schema: SignUpDto): Promise<void> {
+    const { name, email, password } = schema;
+    const user = new UserEntity();
+    user.name = name;
+    user.email = email;
+    user.passwordHash = await this.passwordService.hash(password);
+    await this.userRepository.save(user);
+  }
+
+  public async findByEmail(email: string): Promise<UserEntity | null> {
+    return this.userRepository.findByEmail(email);
+  }
 }
