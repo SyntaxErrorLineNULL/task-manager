7030a1e6f3c72a9c22b1881068b3221109c52bdf
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-03 12:32:07 +0300

    add repository import

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -7,9 +7,10 @@ import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from '../../application/repository/task.repository';
+import CategoryRepository from '../../application/repository/category.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository, CategoryRepository])],
   controllers: [TaskController],
   providers: [TaskService],
 })
