f4e5f18e8bc1f408c68758d29e016a00568ac2b8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-04 23:23:07 +0300

    add database module

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigTypeormService } from '../../../config/config-typeorm.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: ConfigTypeormService,
+    }),
+  ],
   providers: [DatabaseService],
 })
 export class DatabaseModule {}
