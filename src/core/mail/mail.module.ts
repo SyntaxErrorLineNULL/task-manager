7319795db9589b49179f308c38cc9f866c9a6dad
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-04 23:59:20 +0300

    add mailer module

--- src/core/mail/mail.module.ts
+++ src/core/mail/mail.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigMailerService } from '../../../config/config-mailer.service';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useClass: ConfigMailerService,
+    }),
+  ],
   providers: [MailService],
 })
 export class MailModule {}
