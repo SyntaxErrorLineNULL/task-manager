5a76f77b8ff4ba3ece55aac82e88b32226b7419b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-05 20:08:52 +0300

    get all users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,7 +2,16 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { Body, Controller } from '@nestjs/common';
+import { Get, Controller } from '@nestjs/common';
+import UserEntity from '../../application/entity/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly service: UserService) {}
+
+  @Get('users')
+  public async getAllUsers(): Promise<UserEntity[]> {
+    return this.service.getAll();
+  }
+}
