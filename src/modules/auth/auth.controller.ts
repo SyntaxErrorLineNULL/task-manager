c2752cce99de113a44d3810feefc7b6c98942899
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-08 16:57:03 +0300

    update auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,13 +2,21 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignUpDto } from '../common/dto/signUp.dto';
 import { SignInDto } from '../common/dto/signIn.dto';
 import { TokenDto } from '../common/dto/token.dto';
 import UserEntity from '../../application/entity/user.entity';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -23,5 +31,11 @@ export class AuthController {
   @Post('sign-in')
   public async signIn(@Body() body: SignInDto): Promise<TokenDto> {
     return await this.service.signIn(body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('test')
+  public test(@Request() req): any {
+    return req.user;
   }
 }
