ab595f63fa26eccd0f0e337a2212c0c18767b9d2
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-11 22:56:54 +0300

    exports service

--- src/core/mail/mail.service.ts
+++ src/core/mail/mail.service.ts
@@ -1,4 +1,32 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  public async send(
+    email: string,
+    message: string,
+    text: string,
+    html: string,
+  ): Promise<void> {
+    await this.mailerService
+      .sendMail({
+        to: email,
+        subject: message,
+        text: text,
+        html: html,
+      })
+      .then((success) => {
+        console.log(success);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
