f974436ba1cf4ca7e692d82335a1745a050b5667
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-14 15:46:17 +0300

    update path

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -17,7 +17,7 @@ import { TaskService } from './task.service';
 import { CreateTaskDto } from '../common/dto/create.task.dto';
 import TaskEntity from '../../application/entity/task.entity';
 import { ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { JwtAuthGuard } from '../../core/guard/jwt-auth.guard';
 
 @Controller('task')
 @ApiTags('task')
