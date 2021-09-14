00098e950c2bb2d4eac3999540dfc5a0f6c7f0e0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-14 17:26:32 +0300

    update dto

--- src/modules/common/dto/signUp.dto.ts
+++ src/modules/common/dto/signUp.dto.ts
@@ -3,14 +3,20 @@
  */
 
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsEmail } from 'class-validator';
+import { IsPassword } from '../../../core/validator/password.decorator';
 
 export class SignUpDto {
+  @IsNotEmpty()
   @ApiProperty()
   readonly name: string;
 
+  @IsNotEmpty()
+  @IsEmail()
   @ApiProperty()
   readonly email: string;
 
+  @IsPassword()
   @ApiProperty()
   readonly password: string;
 }
