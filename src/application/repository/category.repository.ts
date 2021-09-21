ad5282ce24559251e267b3e87104560078935297
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-21 22:11:29 +0300

    create get by id method

--- src/application/repository/category.repository.ts
+++ src/application/repository/category.repository.ts
@@ -3,10 +3,19 @@
  */
 import { EntityRepository, Repository } from 'typeorm';
 import CategoryEntity from '../entity/category.entity';
+import { NotFoundException } from '@nestjs/common';
 
 @EntityRepository(CategoryEntity)
 export default class CategoryRepository extends Repository<CategoryEntity> {
-  async getAll(): Promise<CategoryEntity[]> {
+  public async getAll(): Promise<CategoryEntity[]> {
     return await this.find({ order: { id: 'DESC' } });
+  }
+
+  public async getById(id: string): Promise<CategoryEntity> {
+    const category = await this.findOne({ where: { id } });
+    if (category === null) {
+      throw new NotFoundException(`Category with ID "${id}" is not found`);
+    }
+    return category;
   }
 }
