7974875c42d83b860ada119455dd467e0616329c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-21 20:39:49 +0300

    #2 #1 create task response collection, check owner task method, refactoring logic

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -5,27 +5,30 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskRepository } from '../../application/repository/task.repository';
-import { CreateTaskDto } from '../common/dto/create.task.dto';
+import { CreateTaskSchema } from '../common/request/create.task.schema';
 import TaskEntity from '../../application/entity/task.entity';
 import CategoryRepository from '../../application/repository/category.repository';
 import UserEntity from '../../application/entity/user.entity';
-import { TaskStatusEnum } from '../../application/entity/task.status.enum';
+import { TaskMapper } from '../common/mapper/task.mapper';
+import { TaskDto } from '../common/dto/task.dto';
+import { TaskCollection } from '../common/dto/task.collection';
 
 @Injectable()
 export class TaskService {
-  constructor(
+  public constructor(
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
     @InjectRepository(CategoryRepository)
     private categoryRepository: CategoryRepository,
+    private readonly taskMapper: TaskMapper,
   ) {}
 
   /**
    * @param schema
    * @param user
    */
-  async createTask(
-    schema: CreateTaskDto,
+  public async createTask(
+    schema: CreateTaskSchema,
     user: UserEntity,
   ): Promise<TaskEntity> {
     const task = new TaskEntity(schema.title, schema.description, user);
@@ -39,35 +42,45 @@ export class TaskService {
     return await this.taskRepository.save(task);
   }
 
-  async getAll(): Promise<TaskEntity[]> {
-    return this.taskRepository.getAllTask();
+  public async getAll(): Promise<TaskCollection> {
+    const tasks = await this.taskRepository.getAllTask();
+    const items = tasks.map((task) => this.taskMapper.mapper(task));
+    return new TaskCollection(items);
   }
 
   /**
    * @param id
    */
-  async getById(id: string): Promise<TaskEntity> {
-    return this.taskRepository.getTaskById(id);
+  public async getById(id: string): Promise<TaskDto> {
+    const task = await this.taskRepository.getTaskById(id);
+    return this.taskMapper.mapper(task);
   }
 
   /**
    * @param id
    * @param user
    */
-  async remove(id: string, user: UserEntity): Promise<void> {
-    const task = await this.getById(id);
+  public async remove(id: string, user: UserEntity): Promise<void> {
+    const task = await this.owner(user, id);
+    await this.taskRepository.removeTask(task.id);
+  }
+
+  public async done(id: string, user: UserEntity): Promise<TaskEntity> {
+    const task = await this.owner(user, id);
+    await task.doneTask();
+    return task;
+  }
+
+  /**
+   * @param user
+   * @param taskId
+   * @private
+   */
+  private async owner(user: UserEntity, taskId: string): Promise<TaskEntity> {
+    const task = await this.taskRepository.getTaskById(taskId);
     if (task.user.id !== user.id) {
       throw new NotFoundException('User is not owner this task');
     }
-    await this.taskRepository.removeTask(id);
-  }
-
-  async done(id: string, user: UserEntity): Promise<TaskEntity> {
-    const task = await this.getById(id);
-    if ((await task).user.id !== user.id) {
-      throw new NotFoundException('User is not owner this task');
-    }
-    task.status = TaskStatusEnum.STATUS_DONE;
     return task;
   }
 }
