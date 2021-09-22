f14b3d25e12132f1bc1b263419b901431b77c7ea
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-22 16:34:00 +0300

    #10 add swagger parameters

--- src/modules/category-task/category-task.controller.ts
+++ src/modules/category-task/category-task.controller.ts
@@ -2,33 +2,66 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, HttpStatus } from '@nestjs/common';
 import { CategoryTaskService } from './category-task.service';
 import CreateCategorySchema from '../common/request/create.category.schema';
 import { CategoryDto } from '../common/dto/category.dto';
 import { CategoryCollection } from '../common/dto/category.collection';
 import { CategoryUpdateSchema } from '../common/request/category.update.schema';
+import { ApiBody, ApiTags, ApiResponse, ApiParam } from '@nestjs/swagger';
 
 @Controller('category')
+@ApiTags('category')
 export class CategoryTaskController {
   public constructor(private service: CategoryTaskService) {}
 
   @Post('create')
+  @ApiBody({ type: [CreateCategorySchema] })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Create category',
+    type: CategoryDto,
+  })
   public async create(@Body() body: CreateCategorySchema): Promise<CategoryDto> {
     return await this.service.create(body);
   }
 
-  @Get('category-tasks')
+  @Get('category-list')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Get all category',
+    type: CategoryCollection,
+  })
   public async getAllCategory(): Promise<CategoryCollection> {
     return await this.service.getAll();
   }
 
   @Post('remove/:id')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Remove category',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    schema: { oneOf: [{ type: 'string' }] },
+  })
   public async remove(@Param('id') id: number): Promise<void> {
     await this.service.remove(id);
   }
 
   @Post('update/:id')
+  @ApiBody({ type: [CategoryUpdateSchema] })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Single category',
+    type: CategoryDto,
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    schema: { oneOf: [{ type: 'string' }] },
+  })
   public async update(
     @Body() body: CategoryUpdateSchema,
     @Param('id') id: number,
@@ -37,6 +70,16 @@ export class CategoryTaskController {
   }
 
   @Get('/:id')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Update category',
+    type: CategoryDto,
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    schema: { oneOf: [{ type: 'string' }] },
+  })
   public async singleCategory(@Param('id') id: number): Promise<CategoryDto> {
     return await this.service.getById(id);
   }
