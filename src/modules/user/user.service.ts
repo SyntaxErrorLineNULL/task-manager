f6c239e8e3b21c16c4269f2e57f74d78d4d5c03f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-09-24 20:16:58 +0300

    #11 refactoring user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,14 +2,13 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from '../../application/repository/user.repository';
 import { SignUpSchema } from '../common/request/signUp.schema';
 import UserEntity from '../../application/entity/user.entity';
 import * as bcrypt from 'bcryptjs';
 import { TokenEntity } from '../../application/entity/token.entity';
-import { UserStatusEnum } from '../../application/entity/user.status.enum';
 
 @Injectable()
 export class UserService {
@@ -18,14 +17,16 @@ export class UserService {
     private userRepository: UserRepository,
   ) {}
 
-  public async createUser(schema: SignUpSchema, token): Promise<UserEntity> {
-    const { name, email, password } = schema;
-    const user = new UserEntity();
-    user.name = name;
-    user.email = email;
-    user.passwordHash = await bcrypt.hashSync(password, 12);
-    const confirmationToken = new TokenEntity();
-    confirmationToken.value = token;
+  public async create(
+    schema: SignUpSchema,
+    confirmationToken: TokenEntity,
+  ): Promise<UserEntity> {
+    const user = new UserEntity(
+      schema.name,
+      schema.email,
+      await bcrypt.hashSync(schema.password, 12),
+    );
+
     user.confirmationToken = confirmationToken;
     return await this.userRepository.save(user);
   }
@@ -44,13 +45,7 @@ export class UserService {
 
   public async confirmationToken(token: string): Promise<void> {
     const user = await this.userRepository.getByToken(token);
-    if (user.confirmationToken.expires > new Date()) {
-      throw new HttpException('Token is expired', HttpStatus.FORBIDDEN);
-    }
-
-    user.confirmationToken.value = null;
-    user.confirmationToken.expires = null;
-    user.status = UserStatusEnum.STATUS_ACTIVE;
+    user.confirmationRegistration(new Date());
     await this.userRepository.save(user);
   }
 }
