cdf2ec8c00cf19b77b26ec3b6ddbd2eb92bbb5b5
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-02 23:23:45 +0300

    #13 password validator

--- src/core/validator/password.decorator.ts
+++ src/core/validator/password.decorator.ts
@@ -2,29 +2,19 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import type { ValidationArguments, ValidationOptions } from 'class-validator';
+import type { ValidationOptions } from 'class-validator';
 import { registerDecorator } from 'class-validator';
+import { PasswordConstraint } from './password.constraint';
 
-export function IsPassword(
-  ValidationOptions?: ValidationOptions,
-): PropertyDecorator {
-  return (object, propertyName: string) => {
+export function IsPassword(ValidationOptions?: ValidationOptions): PropertyDecorator {
+  return (object: any, propertyName: string) => {
     registerDecorator({
       propertyName,
       name: 'IsPassword',
       target: object.constructor,
       constraints: [],
       options: ValidationOptions,
-      validator: {
-        validate(
-          value: string,
-          args?: ValidationArguments,
-        ): Promise<boolean> | boolean {
-          return /^(?=.*\d)(?=.*[A-z])(?=.*[A-z])(?=.*[a-zA-Z]).{5,16}$/.test(
-            value,
-          );
-        },
-      },
+      validator: PasswordConstraint,
     });
   };
 }
