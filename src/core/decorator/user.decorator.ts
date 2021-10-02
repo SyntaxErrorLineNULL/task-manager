8a7caa3098d5985a8464116c5d67c93bb86bc30a
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-02 23:20:05 +0300

    #12 fixed code formatting

--- src/core/decorator/user.decorator.ts
+++ src/core/decorator/user.decorator.ts
@@ -6,9 +6,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 import UserEntity from '../../application/entity/user.entity';
 
-export const Authentication = createParamDecorator(
-  (data: keyof UserEntity, ctx: ExecutionContext) => {
-    const user = ctx.switchToHttp().getRequest<Request>().user as UserEntity;
-    return data ? user && user[data] : user;
-  },
-);
+export const Authentication = createParamDecorator((data: keyof UserEntity, ctx: ExecutionContext) => {
+  const user = ctx.switchToHttp().getRequest<Request>().user as UserEntity;
+  return data ? user && user[data] : user;
+});
