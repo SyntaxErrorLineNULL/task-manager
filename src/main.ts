72505c9af34624c759088cd44f06a551dab0c02f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-15 23:29:05 +0300

    add host name

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { swaggerSetup } from '../config/swagger-setup';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   swaggerSetup(app);
-  await app.listen(process.env.APP_PORT || 5000);
+  await app.listen(process.env.APP_PORT || 5000, '127.0.0.1');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
