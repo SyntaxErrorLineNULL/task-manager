bf711133321a6468f1c130778e8d883ed6684e5a
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-18 21:08:55 +0300

    #5 url generator

---
+++ src/application/service/backend-url-generator.service.ts
@@ -0,0 +1,12 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BackendUrlGeneratorService {
+  constructor(private host: string) {}
+  public build(pathFile: string): string {
+    return this.host + pathFile;
+  }
+}
