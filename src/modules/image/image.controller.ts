3ea8ddb05a06deccaee0b494ee9904eb6048bb17
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-19 20:25:21 +0300

    #5 upload image

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -2,7 +2,32 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { Controller } from '@nestjs/common';
+import { Controller, UploadedFile, Post, UseInterceptors } from '@nestjs/common';
+import { ApiTags, ApiConsumes, ApiBody } from '@nestjs/swagger';
+import { FileService } from '../../application/service/file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerOptions } from '../../../config/multer.service';
 
-@Controller('image')
-export class ImageController {}
+@Controller('/api/image')
+@ApiTags('Image')
+export class ImageController {
+  constructor(private readonly fileService: FileService) {}
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @Post()
+  @UseInterceptors(FileInterceptor('file', multerOptions))
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+}
