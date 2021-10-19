bc6841607d7f5ab5d576472c5bd3834295a1953b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-19 20:24:54 +0300

    #5 image module

--- src/modules/image/image.module.ts
+++ src/modules/image/image.module.ts
@@ -3,6 +3,13 @@
  */
 
 import { Module } from '@nestjs/common';
+import { FileService } from '../../application/service/file.service';
+import { ImageController } from './image.controller';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [ImageController],
+  exports: [],
+  providers: [FileService],
+})
 export class ImageModule {}
