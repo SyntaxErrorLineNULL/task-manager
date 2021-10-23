3c0e2da5d9e64173491d05dd6d1c3b6f35ed8850
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-10-23 23:43:50 +0300

    chat module

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -5,5 +5,6 @@ import { ChatController } from './chat.controller';
 @Module({
   providers: [ChatService],
   controllers: [ChatController],
+  exports: [ChatService],
 })
 export class ChatModule {}
