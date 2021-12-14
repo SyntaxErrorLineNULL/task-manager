d4658108590e3fc42fe3b0f3f128ffd857cf1308
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-12-14 23:28:10 +0300

    use exception handler

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetup } from '../config/swagger-setup';
+import { AuthorizationExceptionHandler } from './components/authorization/authorization.exception-handler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AuthorizationExceptionHandler());
   swaggerSetup(app);
   await app.listen(process.env.APP_PORT || 5000, '127.0.0.1');
   console.log(`Application is running on: ${await app.getUrl()}`);
