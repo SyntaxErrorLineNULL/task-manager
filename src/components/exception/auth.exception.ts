44fef31abe4e10e23ad97419083410598340a9d4
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-01-19 22:04:30 +0300

    wrong registration exception

--- src/components/exception/auth.exception.ts
+++ src/components/exception/auth.exception.ts
@@ -3,7 +3,11 @@
  */
 
 export class AuthorizationException extends Error {
-  public static wrongSignIn() {
+  public static wrongSignIn(): AuthorizationException {
     return new AuthorizationException('Email or password is not correct');
+  }
+
+  public static wrongRegistration(): AuthorizationException {
+    return new AuthorizationException('');
   }
 }
