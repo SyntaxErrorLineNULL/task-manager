77b77bd1614918d7dc0dd716b1dc64de9266695e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-01-29 19:14:39 +0300

    redis config

---
+++ config/redis-main.ts
@@ -0,0 +1,23 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+export const redisMain = {
+  modules: [
+    {
+      host: 'redis1',
+      port: 6379,
+      return_buffers: true,
+    },
+    {
+      host: 'redis2',
+      port: 6379,
+      return_buffers: true,
+    },
+    {
+      host: 'redis3',
+      port: 6379,
+      return_buffers: true,
+    },
+  ],
+};
