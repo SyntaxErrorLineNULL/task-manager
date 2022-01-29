f217d4b2127bd7addb0ed3bb74bc7524b9cfef8c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-01-29 20:24:34 +0300

    create user decorator

--- src/components/guard/decorator/user.decorator.ts
+++ src/components/guard/decorator/user.decorator.ts
@@ -2,4 +2,11 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-export class UserDecorator {}
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { User } from '../../../modules/user/entity/user.entity';
+
+export const userAuthentication = createParamDecorator((data: keyof User, ctx: ExecutionContext) => {
+  const user = ctx.switchToHttp().getRequest<Request>().user as User;
+  return data ? user && user[data] : user;
+});
