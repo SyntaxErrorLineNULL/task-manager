160494befc899b8eaac26d478a6ada91e7f5ace9
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-01-29 21:14:01 +0300

    update user mapper

--- src/modules/common/mapper/user.mapper.ts
+++ src/modules/common/mapper/user.mapper.ts
@@ -6,14 +6,6 @@ import { UserDto } from '../dto/user.dto';
 
 export class UserMapper {
   public mapper(entity: User): UserDto {
-    return new UserDto(
-      entity.id,
-      entity.name,
-      entity.email,
-      entity.createAt.toString(),
-      entity.countTaskComplete,
-      entity.status,
-      entity.role,
-    );
+    return new UserDto(entity.id, entity.name, entity.email, entity.createAt.toString(), entity.status, entity.role);
   }
 }
