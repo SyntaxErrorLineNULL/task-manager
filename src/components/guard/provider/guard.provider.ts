bb5807ab7a8dabab671cd2d252f5d0196d04df04
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-09 17:20:22 +0300

    add new parameter role(auth several type user)

--- src/components/guard/provider/guard.provider.ts
+++ src/components/guard/provider/guard.provider.ts
@@ -2,10 +2,12 @@
  * Author: SyntaxErrorLineNULL.
  */
 
+import { Role } from '../../../modules/user/enum/role';
+
 export const GUARD_DATA_PROVIDER = 'GUARD_DATA_PROVIDER';
 
 export interface GuardProvider {
   provideByEmail(email: string): Promise<any>;
 
-  provideJWTToken(id: string): Promise<any>;
+  provideJWTToken(id: string, roles: Role): Promise<any>;
 }
