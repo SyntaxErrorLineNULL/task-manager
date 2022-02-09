a9ec52113f9d78c7a0bc9f69f9c24f0fb22ea2cf
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-09 17:19:18 +0300

    update sign-up method(remove creating user functional and save token by redis)

--- src/api/auth/user/user-auth.controller.ts
+++ src/api/auth/user/user-auth.controller.ts
@@ -9,8 +9,10 @@ import { RedisService } from '../../../core/store/redis-service';
 import { UserRepository } from '../../../modules/user/entity/user.repository';
 import { AuthorizationException } from '../../../components/exception/auth.exception';
 import { SignUpDto } from '../dto/sign-up.dto';
-import { generateString } from '@nestjs/typeorm';
 import { PasswordService } from '../../../components/guard/service/password.service';
+import { TokenService } from '../../../components/guard/service/token.service';
+import { UserService } from '../../../modules/user/user.service';
+import { CompletionConfirmationDto } from '../dto/completion-confirmation.dto';
 
 @Controller('api/user/auth')
 export class UserAuthController {
@@ -19,6 +21,8 @@ export class UserAuthController {
     private redis: RedisService,
     private userRepository: UserRepository,
     private passwordService: PasswordService,
+    private tokenService: TokenService,
+    private userService: UserService,
   ) {}
 
   @Post('sign-up')
@@ -26,20 +30,13 @@ export class UserAuthController {
     if (await this.userRepository.findOne({ email: body.email })) {
       throw AuthorizationException.wrongRegistration();
     }
+    await this.userService.createUser(body);
+    await this.tokenService.sendConfirmationToken(body.email, body.name);
+  }
 
-    const user = await this.userRepository.create({
-      id: generateString(),
-      name: body.name,
-      email: body.email,
-      passwordHash: await this.passwordService.hash(body.password),
-    });
-    await this.userRepository.save(user);
-    const date = new Date();
-    const token = {
-      email: 'email@thiss.com',
-      expires: date.setHours(date.getHours() + 2),
-    };
-    await this.redis.redisClient.set('123456', JSON.stringify(token));
+  @Post('confirmation')
+  public async completionConfirmation(@Body() body: CompletionConfirmationDto) {
+    await this.tokenService.completionConfirmation(body.token);
   }
 
   @Post('sign-in')
