37f27c35f6298faf4d8e0b3939acafaa2e20472f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-09 17:08:39 +0300

    check status

--- src/api/auth/user/user-guard.provider.ts
+++ src/api/auth/user/user-guard.provider.ts
@@ -5,14 +5,24 @@ import { GuardProvider } from '../../../components/guard/provider/guard.provider
 import { UserRepository } from '../../../modules/user/entity/user.repository';
 import { Injectable } from '@nestjs/common';
 import { Role } from '../../../modules/user/enum/role';
+import { UserStatusEnum } from '../../../modules/user/enum/user.status.enum';
+import { AuthorizationException } from '../../../components/exception/auth.exception';
+
+export interface UserDataProvider {
+  id: string;
+  roles: string[];
+  email: string;
+  passwordHash: string;
+}
 
 @Injectable()
 export class UserGuardProvider implements GuardProvider {
   constructor(private userRepository: UserRepository) {}
 
-  async provideByEmail(email: string): Promise<any> {
+  async provideByEmail(email: string): Promise<UserDataProvider> {
     const user = await this.userRepository.findOne({ email });
     if (!user) return null;
+    if (user.status === UserStatusEnum.STATUS_BLOCKED) throw AuthorizationException.wrongBlockedUser();
     return {
       id: user.id,
       roles: [user.role],
@@ -21,10 +31,10 @@ export class UserGuardProvider implements GuardProvider {
     };
   }
 
-  async provideJWTToken(id: string): Promise<any> {
+  async provideJWTToken(id: string, roles: Role): Promise<any> {
     return {
       userId: id,
-      roles: [Role.USER],
+      roles: [roles],
     };
   }
 }
