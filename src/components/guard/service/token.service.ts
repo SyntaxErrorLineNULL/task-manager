13480da1a8196bdb31f33df884ef8be2730c6a8b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-10 22:36:50 +0300

    remove time parameter

--- src/components/guard/service/token.service.ts
+++ src/components/guard/service/token.service.ts
@@ -11,7 +11,6 @@ import { UserStatusEnum } from '../../../modules/user/enum/user.status.enum';
 
 export interface ConfirmationOptions {
   email: string;
-  expires: number;
 }
 
 @Injectable()
@@ -19,11 +18,9 @@ export class TokenService {
   constructor(private mailService: MailService, private redis: RedisService, private userRepository: UserRepository) {}
 
   public async sendConfirmationToken(email: string, name: string): Promise<void> {
-    const date = new Date();
     const token = Math.random().toString(36).substring(2, 9);
     const tokenOption: ConfirmationOptions = {
       email: email,
-      expires: date.setHours(date.getHours() + 2),
     };
     await this.redis.redisClient.set(token.toString(), JSON.stringify(tokenOption), 'EX', 3600);
 
@@ -38,12 +35,10 @@ export class TokenService {
       throw AuthorizationException.wrongCompletionConfirmation();
 
     const token = JSON.parse(await this.redis.redisClient.get(confirmationToken));
-    if (token.expires < Date.now()) throw AuthorizationException.wrongCompletionConfirmation();
 
     const user = await this.userRepository.findByEmail(token.email);
     if (!user) throw AuthorizationException.wrongSignIn();
     user.changeStatus(UserStatusEnum.STATUS_ACTIVE);
     await this.userRepository.save(user);
-    await this.redis.redisClient.del(confirmationToken);
   }
 }
