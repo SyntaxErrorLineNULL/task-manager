4525204c41775d10202a9cad352fd8723ab4027f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-13 22:46:25 +0300

    other service client

--- service/token-service/src/token.module.ts
+++ service/token-service/src/token.module.ts
@@ -7,6 +7,7 @@ import * as redisStore from 'cache-manager-redis-store';
 import { CacheModule, Module } from '@nestjs/common';
 import { TokenController } from './token.controller';
 import { TokenService } from './token-service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -17,6 +18,22 @@ import { TokenService } from './token-service';
         port: 6379,
       },
     }),
+    ClientsModule.register([
+      {
+        name: 'MAIL_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: 'nats://localhost:4222',
+        },
+      },
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: 'nats://localhost:4222',
+        },
+      },
+    ]),
   ],
   controllers: [TokenController],
   providers: [TokenService],
