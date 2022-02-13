2a994936b707ca1651c13a661cc9cc88ac399ab2
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-13 22:45:47 +0300

    token sent exception

--- service/token-service/src/infrastructure/exception/token.exception.ts
+++ service/token-service/src/infrastructure/exception/token.exception.ts
@@ -6,4 +6,8 @@ export class TokenException extends Error {
   public static tokenIsNotExists(): TokenException {
     return new TokenException('This token is not found or not valid');
   }
+
+  public static tokenSent(): TokenException {
+    return new TokenException('Time to send a new token has not passed');
+  }
 }
