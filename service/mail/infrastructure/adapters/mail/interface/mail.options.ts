5388149eb043bca858a637dcbd5b1bd608c7bbb9
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-17 23:46:06 +0300

    update adapter

--- service/mail/infrastructure/adapters/mail/interface/mail.options.ts
+++ service/mail/infrastructure/adapters/mail/interface/mail.options.ts
@@ -3,10 +3,15 @@
  */
 
 import { TransportType } from './transport.type';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import * as SMTPPool from 'nodemailer/lib/smtp-pool';
+import * as SendmailTransport from 'nodemailer/lib/sendmail-transport';
+import * as StreamTransport from 'nodemailer/lib/stream-transport';
 
 export const MAIL_OPTIONS = 'MAIL_OPTIONS';
 
 export interface MailOptions {
+  defaults?: SMTPTransport.Options | SMTPPool.Options | SendmailTransport.Options | StreamTransport.Options;
   transport: TransportType;
   template?: {
     dir?: string;
--- service/mail/infrastructure/adapters/mail/mail-adapter.ts
+++ service/mail/infrastructure/adapters/mail/mail-adapter.ts
@@ -4,12 +4,13 @@
 
 import { Inject, Injectable } from '@nestjs/common';
 import { MAIL_OPTIONS, MailOptions } from './interface/mail.options';
-import { SentMessageInfo, Transporter } from 'nodemailer';
+import { createTransport, Transporter } from 'nodemailer';
 import * as SMTPTransport from 'nodemailer/lib/smtp-transport';
 import * as SMTPPool from 'nodemailer/lib/smtp-pool';
 import * as SendmailTransport from 'nodemailer/lib/sendmail-transport';
 import * as StreamTransport from 'nodemailer/lib/stream-transport';
 import Mail from 'nodemailer/lib/mailer';
+import { options } from 'tsconfig-paths/lib/options';
 
 export type TransportType = SMTPTransport | SMTPPool | SendmailTransport | StreamTransport | string;
 
@@ -18,10 +19,15 @@ export class MailAdapter {
   private transporter: Transporter;
 
   protected constructor(@Inject(MAIL_OPTIONS) private mailOptions: MailOptions) {
-    if (!mailOptions.transport) throw new Error('Transport configuration is not found');
+    if (!mailOptions.transport || Object.keys(mailOptions.transport).length <= 0)
+      throw new Error('Transport configuration is not found');
+
+    this.transporter = this.createMailTransport(mailOptions);
   }
 
-  private createTransport(options: TransportType): any {}
+  private createMailTransport(options: MailOptions): Mail {
+    return createTransport(options.transport, options.defaults);
+  }
 
   public async sendMail(options: MailOptions) {}
 }
