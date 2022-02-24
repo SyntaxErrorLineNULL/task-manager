0b11ebdcd6a45af4ee75cca19a7ed8698d0948c2
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-24 21:43:16 +0300

    update handlebars adapter, precompile template functional, coding compile template method

--- service/mail/adapters/template/handlebars.adapter.ts
+++ service/mail/adapters/template/handlebars.adapter.ts
@@ -2,4 +2,53 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-export class HandlebarsAdapter {}
+import { HelperDeclareSpec } from 'handlebars';
+import handlebars from 'handlebars';
+import path from 'path';
+import { get } from 'lodash';
+import fs from 'fs';
+import { MailOptions } from '../mail/interface/mail.options';
+
+export class HandlebarsAdapter {
+  private precompiledTemplates: {
+    [name: string]: handlebars.TemplateDelegate;
+  } = {};
+
+  constructor(option?: HelperDeclareSpec) {
+    handlebars.registerHelper('concat', (...args) => {
+      args.pop();
+      return args.join('');
+    });
+    handlebars.registerHelper(option || {});
+  }
+
+  public compileTemplate(mail: any, callback: any, mailerOptions: MailOptions) {
+    const { templateName } = this.precompile(mail.data.template, callback, mailerOptions.template);
+    const templateOptions = get(mailerOptions, 'options', { functional: false, data: {} });
+  }
+
+  private precompile(
+    template: any,
+    callback: any,
+    options: any,
+  ): { templateExt: string; templateName: string; templateDir: string; templatePath: string } {
+    const templateExt = path.extname(template) || '.hbs';
+    const templateName = path.basename(template, path.extname(template));
+    const templateDir = path.isAbsolute(template)
+      ? path.dirname(template)
+      : path.join(get(options, 'dir', ''), path.dirname(template));
+    const templatePath = path.join(templateDir, templateName + templateExt);
+
+    if (!this.precompiledTemplates) {
+      try {
+        const template = fs.readFileSync(templatePath, 'utf-8');
+
+        this.precompiledTemplates[templateName] = handlebars.compile(template, get(options, 'options', {}));
+      } catch (error) {
+        return callback(error);
+      }
+    }
+
+    return { templateExt, templateName, templateDir, templatePath };
+  }
+}
