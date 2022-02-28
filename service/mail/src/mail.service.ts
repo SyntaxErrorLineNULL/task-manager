1c7cc2e3f123380a1f0b7446f4d41084a0e3b746
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-02-28 22:27:33 +0300

    send method

--- service/mail/src/mail.service.ts
+++ service/mail/src/mail.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { MailAdapter } from '../adapters/mail/mail-adapter';
 
 @Injectable()
 export class MailService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private mailerService: MailAdapter) {}
+
+  public async send(email: string, message: string): Promise<void> {
+    await this.mailerService
+      .sendMail({
+        to: email,
+        subject: message,
+        html: '<b>Hello world?</b>',
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
