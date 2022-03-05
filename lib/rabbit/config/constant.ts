1713c8cd4910c15cd24df3ee9fac63579084d8ca
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-03-05 22:00:26 +0300

    rabbit config constant

---
+++ lib/rabbit/config/constant.ts
@@ -0,0 +1,19 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+export enum Keys {
+  SEND_SMS = 'notify.sendSms',
+  SEND_TO_DELIVERY = 'integrates.delivery',
+  HOLD = 'hold',
+}
+
+export enum Types {
+  QUEUE = 'queue',
+  EXCHANGE = 'exchange',
+}
+
+export enum ExchangeTypes {
+  TOPIC = 'topic',
+  DIRECT = 'direct',
+}
