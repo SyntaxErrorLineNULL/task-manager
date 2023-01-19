a591a0d97b587c124196feeda071085976a2cdb0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-01-19 23:27:50 +0300

    init storage method

--- user/utils/context/storage/AsynchronousLocalStorage.js
+++ user/utils/context/storage/AsynchronousLocalStorage.js
@@ -21,4 +21,23 @@ export class AsynchronousLocalStorage {
   constructor () {
     this.#asyncLocaleStorage = new AsyncLocalStorage();
   }
+
+  /**
+   * @description Runs a function synchronously within a context and returns its return value.
+   * The store is not accessible outside the callback function.
+   * The store is accessible to any asynchronous operations created within the callback.
+   * The optional args are passed to the callback function.
+   * @link https://nodejs.org/api/async_context.html#asynclocalstoragerunstore-callback-args
+   * @param { Object } context
+   * @param { callback } callback
+   * @returns { * }
+   */
+  async initStorage (context, callback) {
+    const store = context ? new Map(Object.entries(context)) : new Map();
+
+    this.#asyncLocaleStorage.run(store, () => {
+      callback();
+    });
+  }
+
 }
