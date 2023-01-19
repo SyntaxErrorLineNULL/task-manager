76e4c925eade89b0cb35265bd86dbd00ba816ad8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-01-19 23:23:57 +0300

    init async local storage wrapper

---
+++ user/utils/context/storage/AsynchronousLocalStorage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+ * AsynchronousLocalStorage
+ *
+ * (c) SyntaxErrorLineNULL
+ */
+
+import { AsyncLocalStorage } from 'async_hooks';
+
+/**
+ * @export
+ * @class AsynchronousLocalStorage
+ */
+export class AsynchronousLocalStorage {
+
+}
