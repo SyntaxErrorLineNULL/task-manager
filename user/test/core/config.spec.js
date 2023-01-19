c8fb2c1f638d90e5a8e77bb4952c1c5b185aabbd
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-01-19 20:50:04 +0300

    config unit test

---
+++ user/test/core/config.spec.js
@@ -0,0 +1,17 @@
+import { Config } from '../../core/config.js';
+import path from 'node:path';
+import { describe, expect, test } from '@jest/globals';
+
+describe('test config loader', () => {
+  test('test config', () => {
+    const filePath = path.resolve(process.cwd(), './.env.test');
+    const config = new Config(filePath);
+    expect(config.getParameter('APP_NAME')).toBe('test-application');
+    expect(config.getParameter('PORT')).toBe('3000');
+    expect(config.getParameter('DEBUG')).toBe('true');
+    expect(config.getParameter('EMPTY_DATA')).toBe('``');
+    expect(config.getParameter('SINGLE_QUOTES_SPACED')).toBe('    single quotes    ');
+    expect(config.getParameter('DOUBLE_QUOTES')).toBe('double_quotes');
+    expect(config.getParameter('DOUBLE_QUOTES_SPACED')).toBe('    double quotes    ');
+  });
+});
