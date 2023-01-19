8087294662eb8fb721ba675803bf20d083d1a0ad
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-01-19 23:59:46 +0300

    update test config loader

--- user/test/core/config.spec.js
+++ user/test/core/config.spec.js
@@ -1,17 +1,19 @@
 import { Config } from '../../core/config.js';
 import path from 'node:path';
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, it, test } from '@jest/globals';
 
 describe('test config loader', () => {
-  test('test config', () => {
-    const filePath = path.resolve(process.cwd(), './.env.test');
-    const config = new Config(filePath);
-    expect(config.getParameter('APP_NAME')).toBe('test-application');
-    expect(config.getParameter('PORT')).toBe('3000');
-    expect(config.getParameter('DEBUG')).toBe('true');
-    expect(config.getParameter('EMPTY_DATA')).toBe('``');
-    expect(config.getParameter('SINGLE_QUOTES_SPACED')).toBe('    single quotes    ');
-    expect(config.getParameter('DOUBLE_QUOTES')).toBe('double_quotes');
-    expect(config.getParameter('DOUBLE_QUOTES_SPACED')).toBe('    double quotes    ');
-  });
+  describe('', () => {
+    it('', () => {
+      const filePath = path.resolve(process.cwd(), './.env.test');
+      const config = new Config(filePath);
+      expect(config.getParameter('APP_NAME')).toBe('test-application');
+      expect(config.getParameter('PORT')).toBe('3000');
+      expect(config.getParameter('DEBUG')).toBe('true');
+      expect(config.getParameter('EMPTY_DATA')).toBe('``');
+      expect(config.getParameter('SINGLE_QUOTES_SPACED')).toBe('    single quotes    ');
+      expect(config.getParameter('DOUBLE_QUOTES')).toBe('double_quotes');
+      expect(config.getParameter('DOUBLE_QUOTES_SPACED')).toBe('    double quotes    ');
+    })
+  })
 });
