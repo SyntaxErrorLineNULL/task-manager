b9960c665d38eb1c1917124d39fd800b1cb6bea7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-01-19 23:53:17 +0300

    undefined unit test

--- user/test/utils/als.spec.js
+++ user/test/utils/als.spec.js
@@ -2,9 +2,11 @@ import { describe, expect, it } from '@jest/globals';
 import { AsynchronousLocalStorage } from '../../utils/context/storage/AsynchronousLocalStorage.js';
 
 describe('async local storage', () => {
-  it('undefined', () => {
-    const als = new AsynchronousLocalStorage();
-    als.set('key', 'value');
-    expect(als.get('key')).toBeUndefined();
+  const als = new AsynchronousLocalStorage();
+  describe('set is called without running storage', () => {
+    it('undefined', () => {
+      als.set('key', 'value');
+      expect(als.get('key')).toBeUndefined();
+    });
   });
 });
