7bbfebb579e1a270945825663dd1118b16631426
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-02-01 23:32:19 +0300

    create method publish

--- user/core/PubSub.js
+++ user/core/PubSub.js
@@ -1,14 +1,36 @@
 'use strict';
 
-import { connect } from "nats";
+import { connect, StringCodec } from 'nats';
 
 export class PubSub {
+  #subject;
   #client;
-  constructor (option) {
+
+  #logger;
+  constructor (option, logger) {
     this.#client = connect(option);
+    this.#logger = logger;
   }
 
-  Publish () {}
+  Publish (data) {
+    if (data === null || data === undefined) {
+      this.#logger.error();
+      // TODO: create error
+    }
+    const sc = StringCodec();
+    return new Promise((resolve, reject) => {
+      this.#client.publish(this.#subject, sc.encode(String(data)), (err) => {
+        if (err) {
+          this.#logger.error(`failed to publish: ${err}`);
+          return reject(err);
+        }
+        this.#logger.info('successful published');
+        resolve();
+      });
+    });
+  }
 
   Subscribe () {}
+
+  #closed () {}
 }
