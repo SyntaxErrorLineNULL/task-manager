699031f4fd622cf7883dab2934a64aa54f5dbecc
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-05-01 01:07:08 +0300

    update annotation

--- user/domain/repository/user-repository.js
+++ user/domain/repository/user-repository.js
@@ -18,18 +18,41 @@ export class UserRepository {
 
     /**
      * @param { User } user
-     * @returns {Promise<void>}
+     * @returns {Promise<void>|Error}
      * @constructor
      */
     async Create(user) {}
 
+    /**
+     *
+     * @param { User } user
+     * @returns {Promise<void>|Error}
+     * @constructor
+     */
     async Update(user) {}
 
+    /**
+     *
+     * @param { User } user
+     * @returns {Promise<User>|Error}
+     * @constructor
+     */
     async Find(user) {}
 
+    /**
+     * @param { string } id
+     * @returns {Promise<void>|Error}
+     * @constructor
+     */
     async Delete(id) {}
 
-    async CheckExists() {}
+    /**
+     *
+     * @param { string } query
+     * @returns {Promise<boolean>|Error}
+     * @constructor
+     */
+    async CheckExists(query) {}
 
     #createTable() {
         this.#database.schema.createTableIfNotExists('user', (tableBuilder) => {
