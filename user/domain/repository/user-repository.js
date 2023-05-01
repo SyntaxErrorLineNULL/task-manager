7ad2a27c5e334792f07a24f3df378ccb454803b1
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-05-01 23:04:57 +0300

    create user method implement

--- user/domain/repository/user-repository.js
+++ user/domain/repository/user-repository.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/**
+ * @description ErrEmpty returns when request was with empty data
+ * @type {Error}
+ */
+const ErrEmpty = new Error('empty user data')
+
+/**
+ * @description ErrInternal returns when something went wrong in repository
+ * @type {Error}
+ */
+const ErrInternal = new Error('internal error')
+
 export class UserRepository {
     /**
      * @param { knex.Client } database
@@ -21,7 +33,22 @@ export class UserRepository {
      * @returns {Promise<void>|Error}
      * @constructor
      */
-    async Create(user) {}
+    async Create(user) {
+        this.#logger.Info('Create new user');
+        if (user === null) {
+            this.#logger.Error('user is null', { duration: Date.now() });
+            throw ErrEmpty;
+        }
+
+        try {
+            const command = `INSERT INTO "user" (id, email, username, first_name, last_name, password) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+            await this.#database.raw(command, [user.ID, user.Email, user.UserName, user.FirstName, user.LastName, user.Password]);
+            this.#logger.Info('user created successfully');
+        } catch (err) {
+            this.#logger.Error('insert error', { duration: Date.now(), error: err.message });
+            throw ErrInternal;
+        }
+    }
 
     /**
      *
@@ -37,7 +64,9 @@ export class UserRepository {
      * @returns {Promise<User>|Error}
      * @constructor
      */
-    async Find(user) {}
+    async Find(user) {
+
+    }
 
     /**
      * @param { string } id
