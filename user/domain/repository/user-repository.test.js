2b26a0eef54a760f90419e6612e3f1a54d090668
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-05-02 23:55:28 +0300

    create table test

--- user/domain/repository/user-repository.test.js
+++ user/domain/repository/user-repository.test.js
@@ -1,5 +1,6 @@
 import { knex } from 'knex';
-import { beforeAll, describe } from "@jest/globals";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, jest } from '@jest/globals';
+import { UserRepository } from './user-repository.js';
 
 describe('UserRepository test', () => {
     let repo;
@@ -16,5 +17,30 @@ describe('UserRepository test', () => {
                 database: 'app',
             },
         });
+    });
+
+    afterAll(() => {
+        database.destroy();
+    });
+
+    beforeEach(async () => {
+        await database.schema.createTable('users', (table) => {
+            table.uuid('id').primary();
+            table.string('email', 128).unique();
+            table.string('username', 64).unique();
+            table.string('first_name', 64);
+            table.string('last_name', 64);
+            table.text('password', 'longtext');
+            table.timestamp('created_at').notNullable().defaultTo(database.fn.now());
+        });
+
+        repo = new UserRepository(database, {
+            Info: jest.fn(),
+            Error: jest.fn(),
+        });
+    });
+
+    afterEach(async () => {
+        await database.schema.dropTable('users');
     });
 })
