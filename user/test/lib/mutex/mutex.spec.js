e3cb60be85b037b3f6dd93e0ee83938808225fa1
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-05-17 13:24:08 +0300

    create mutex test

--- user/test/lib/mutex/mutex.spec.js
+++ user/test/lib/mutex/mutex.spec.js
@@ -2,9 +2,24 @@ import { describe, expect, it } from '@jest/globals';
 import { Mutex } from '../../../lib/Mutex/Mutex';
 
 describe('Mutex', () => {
-  it('should allow acquiring and releasing the lock', async () => {
-    const mutex = new Mutex();
+  let mutex;
 
+  beforeEach(() => {
+    mutex = new Mutex();
+  });
+
+  afterEach(() => {
+    mutex = null;
+  });
+
+  it('should acquire and release the lock', async () => {
+    await mutex.acquire();
+    expect(mutex.isLocked()).toBe(true);
+    mutex.release();
+    expect(mutex.isLocked()).toBe(false);
+  });
+
+  it('should allow acquiring and releasing the lock', async () => {
     // Acquire the lock
     await mutex.acquire();
     expect(mutex.isLocked()).toBe(true);
@@ -17,6 +32,50 @@ describe('Mutex', () => {
   it('throw error if unlocking unlocked mutex', () => {
     const mutex = new Mutex();
     expect(() => mutex.release()).toThrowError('Mutex is not locked');
+  });
+
+  it('should allow multiple acquires and releases',  () => {
+    mutex.acquire();
+    expect(mutex.isLocked()).toBe(true);
+
+    mutex.acquire();
+    expect(mutex.isLocked()).toBe(true);
+
+    mutex.release();
+    expect(mutex.isLocked()).toBe(true);
+
+    mutex.release();
+    expect(mutex.isLocked()).toBe(false);
+  });
+
+  it('should queue promises and release them in order', async () => {
+    const order = [];
+    // Acquire the lock for the first promise
+    Promise.all([
+      mutex.acquire(),
+      order.push(1),
+
+      mutex.acquire(),
+      order.push(2),
+
+      mutex.acquire(),
+      order.push(3),
+
+      mutex.acquire(),
+      order.push(4),
+    ])
+
+    // Wait briefly to ensure the queued promises haven't been released yet
+    new Promise((resolve) => setTimeout(resolve, 100));
+
+    // The lock should still be held by the first promise
+    expect(mutex.isLocked()).toBe(true);
+    // Release the lock, allowing the queued promises to acquire it in order
+    mutex.release();
+
+    // All promises should have been released in order
+    expect(order).toEqual([1, 2, 3, 4]);
+    expect(mutex.isLocked()).toBe(true);
   });
 });
 
